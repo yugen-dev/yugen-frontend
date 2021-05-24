b41d3563df12dd51f1da8087f261dd28e335cc73
samarth30 <samarth.gugnani30@gmail.com>
2021-05-24 16:11:51 +0530

    gasless approve added

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,8 +1,11 @@
 import { ChainId, JSBI, Percent, Token, WETH } from "@pancakeswap-libs/sdk";
 
+import tokenABI from "./abis/token.json";
+import usdcABI from "./abis/usdc.json";
+
 export const ROUTER_ADDRESS = "0x3fd7a9b75154471a7A7c24103BcBb54485eEA345";
 
-export const biconomyAPIKey = "G1XvRw8LY.249aeb58-fd1b-4c38-a224-d50387e62d42";
+export const biconomyAPIKey = "b8dPfq5r9.4828e19b-d67b-454b-823d-da8c44e83da4";
 export const META_TXN_DISABLED = false;
 
 // a list of tokens by chain
@@ -102,6 +105,42 @@ export const PINNED_PAIRS: {
     [BUSD, USDT],
     [DAI, USDT],
   ],
+};
+
+export const META_TXN_SUPPORTED_TOKENS: any = {
+  "0xc2132d05d31c914a87c6611c10748aeb04b58e8f": { abi: tokenABI },
+  "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619": { abi: tokenABI },
+  "0x2791bca1f2de4661ed88a30c99a7a9449aa84174": { abi: usdcABI },
+  "0x8505b9d2254a7ae468c0e9dd10ccea3a837aef5c": { abi: tokenABI },
+  "0x313d009888329c9d1cf4f75ca3f32566335bd604": { abi: tokenABI },
+  "0xda537104d6a5edd53c6fbba9a898708e465260b6": { abi: tokenABI },
+  "0x8f3cf7ad23cd3cadbd9735aff958023239c6a063": { abi: tokenABI },
+  "0xdab529f40e671a1d4bf91361c21bf9f0c9712ab7": { abi: tokenABI },
+  "0xa1c57f48f0deb89f569dfbe6e2b7f46d33606fd4": { abi: tokenABI },
+  "0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6": { abi: tokenABI },
+  "0x71b821aa52a49f32eed535fca6eb5aa130085978": { abi: tokenABI },
+  "0x578360adf0bbb2f10ec9cec7ef89ef495511ed5f": { abi: tokenABI },
+  "0x556f501cf8a43216df5bc9cc57eb04d4ffaa9e6d": { abi: tokenABI },
+  "0xeab9cfb094db203e6035c2e7268a86debed5bd14": { abi: tokenABI },
+  "0xb33eaad8d922b1083446dc23f610c2567fb5180f": { abi: tokenABI },
+  "0x5a2fdf906ada9353ebe496fa5d351b39f8908d19": { abi: tokenABI },
+  "0xe6fc6c7cb6d2c31b359a49a33ef08ab87f4de7ce": { abi: tokenABI },
+  "0x16eccfdbb4ee1a85a33f3a9b21175cd7ae753db4": { abi: tokenABI },
+  "0x03247a4368a280bec8133300cd930a3a61d604f6": { abi: tokenABI },
+  "0x840195888db4d6a99ed9f73fcd3b225bb3cb1a79": { abi: tokenABI },
+  "0x53e0bca35ec356bd5dddfebbd1fc0fd03fabad39": { abi: tokenABI },
+  "0x385eeac5cb85a38a9a07a70c73e0a3271cfb54a7": { abi: tokenABI },
+  "0x462d8d82c2b2d2ddabf7f8a93928de09d47a5807": { abi: tokenABI },
+  "0x72d6066f486bd0052eefb9114b66ae40e0a6031a": { abi: tokenABI },
+  "0xc3ec80343d2bae2f8e680fdadde7c17e71e114ea": { abi: tokenABI },
+  "0xc91c06db0f7bffba61e2a5645cc15686f0a8c828": { abi: tokenABI },
+  "0x4fdce518fe527439fe76883e6b51a1c522b61b7c": { abi: tokenABI },
+  "0xa3ed22eee92a3872709823a6970069e12a4540eb": { abi: tokenABI },
+  "0xbe5cf150e1ff59ca7f2499eaa13bfc40aae70e78": { abi: tokenABI },
+  "0xf6f85b3f9fd581c2ee717c404f7684486f057f95": { abi: tokenABI },
+  "0x40ccd55b789fdee8d434915dc2aa6bd938506a92": { abi: tokenABI },
+  "0xa79e0bfc579c709819f4a0e95d4597f03093b011": { abi: tokenABI },
+  "0x6a297b46455a8a48664da92f7fa423219fabb355": { abi: tokenABI },
 };
 
 export const NetworkContextName = "NETWORK";
--- src/hooks/useApproveCallback.ts
+++ src/hooks/useApproveCallback.ts
@@ -1,16 +1,48 @@
-/* eslint-disable no-nested-ternary */
-import { MaxUint256 } from '@ethersproject/constants'
-import { TransactionResponse } from '@ethersproject/providers'
-import { Trade, TokenAmount, CurrencyAmount, ETHER } from '@pancakeswap-libs/sdk'
-import { useCallback, useMemo } from 'react'
-import { ROUTER_ADDRESS } from '../constants'
-import { useTokenAllowance } from '../data/Allowances'
-import { Field } from '../state/swap/actions'
-import { useTransactionAdder, useHasPendingApproval } from '../state/transactions/hooks'
-import { computeSlippageAdjustedAmounts } from '../utils/prices'
-import { calculateGasMargin } from '../utils'
-import { useTokenContract } from './useContract'
-import { useActiveWeb3React } from './index'
+import { MaxUint256 } from "@ethersproject/constants";
+import { TransactionResponse } from "@ethersproject/providers";
+import {
+  Trade,
+  TokenAmount,
+  CurrencyAmount,
+  ETHER,
+} from "@pancakeswap-libs/sdk";
+import { Biconomy } from "@biconomy/mexa";
+import { splitSignature } from "@ethersproject/bytes";
+import Web3 from "web3";
+import { AbiItem } from "web3-utils";
+
+import { useCallback, useMemo } from "react";
+import {
+  ROUTER_ADDRESS,
+  biconomyAPIKey,
+  META_TXN_DISABLED,
+  META_TXN_SUPPORTED_TOKENS,
+} from "../constants";
+import tokenABI from "../constants/abis/token.json";
+import { useTokenAllowance } from "../data/Allowances";
+import { Field } from "../state/swap/actions";
+import {
+  useTransactionAdder,
+  useHasPendingApproval,
+} from "../state/transactions/hooks";
+import { computeSlippageAdjustedAmounts } from "../utils/prices";
+import { calculateGasMargin } from "../utils";
+import { useTokenContract } from "./useContract";
+import { useActiveWeb3React } from "./index";
+
+const contractAddress = ROUTER_ADDRESS;
+const maticProvider = process.env.REACT_APP_NETWORK_URL;
+console.log(maticProvider);
+// @ts-ignore
+const biconomy = new Biconomy(new Web3.providers.HttpProvider(maticProvider), {
+  apiKey: biconomyAPIKey,
+  debug: true,
+});
+console.log(biconomy);
+const getWeb3 = new Web3(biconomy);
+biconomy.onEvent(biconomy.READY, () => {
+  console.log("Mexa is Ready");
+});
 
 export enum ApprovalState {
   UNKNOWN,
@@ -24,86 +56,199 @@ export function useApproveCallback(
   amountToApprove?: CurrencyAmount,
   spender?: string
 ): [ApprovalState, () => Promise<void>] {
-  const { account } = useActiveWeb3React()
-  const token = amountToApprove instanceof TokenAmount ? amountToApprove.token : undefined
-  const currentAllowance = useTokenAllowance(token, account ?? undefined, spender)
-  const pendingApproval = useHasPendingApproval(token?.address, spender)
+  const { chainId, library, account } = useActiveWeb3React();
+  const token =
+    amountToApprove instanceof TokenAmount ? amountToApprove.token : undefined;
+  const currentAllowance = useTokenAllowance(
+    token,
+    account ?? undefined,
+    spender
+  );
+  const pendingApproval = useHasPendingApproval(token?.address, spender);
 
   // check the current approval status
   const approvalState: ApprovalState = useMemo(() => {
-    if (!amountToApprove || !spender) return ApprovalState.UNKNOWN
-    if (amountToApprove.currency === ETHER) return ApprovalState.APPROVED
+    if (!amountToApprove || !spender) return ApprovalState.UNKNOWN;
+    if (amountToApprove.currency === ETHER) return ApprovalState.APPROVED;
     // we might not have enough data to know whether or not we need to approve
-    if (!currentAllowance) return ApprovalState.UNKNOWN
+    if (!currentAllowance) return ApprovalState.UNKNOWN;
 
     // amountToApprove will be defined if currentAllowance is
+    // eslint-disable-next-line no-nested-ternary
     return currentAllowance.lessThan(amountToApprove)
       ? pendingApproval
         ? ApprovalState.PENDING
         : ApprovalState.NOT_APPROVED
-      : ApprovalState.APPROVED
-  }, [amountToApprove, currentAllowance, pendingApproval, spender])
+      : ApprovalState.APPROVED;
+  }, [amountToApprove, currentAllowance, pendingApproval, spender]);
 
-  const tokenContract = useTokenContract(token?.address)
-  const addTransaction = useTransactionAdder()
+  const tokenContract = useTokenContract(token?.address);
+  const addTransaction = useTransactionAdder();
 
   const approve = useCallback(async (): Promise<void> => {
     if (approvalState !== ApprovalState.NOT_APPROVED) {
-      console.error('approve was called unnecessarily')
-      return
+      console.error("approve was called unnecessarily");
+      return;
     }
     if (!token) {
-      console.error('no token')
-      return
+      console.error("no token");
+      return;
     }
 
     if (!tokenContract) {
-      console.error('tokenContract is null')
-      return
+      console.error("tokenContract is null");
+      return;
     }
 
     if (!amountToApprove) {
-      console.error('missing amount to approve')
-      return
+      console.error("missing amount to approve");
+      return;
     }
 
     if (!spender) {
-      console.error('no spender')
-      return
+      console.error("no spender");
+      return;
     }
+    const estimatedGas = await tokenContract.estimateGas
+      .approve(spender, MaxUint256)
+      .catch(() => {
+        // general fallback for tokens who restrict approval amounts
+        useExact = true;
+        return tokenContract.estimateGas.approve(
+          spender,
+          amountToApprove.raw.toString()
+        );
+      });
 
-    let useExact = false
-    const estimatedGas = await tokenContract.estimateGas.approve(spender, MaxUint256).catch(() => {
-      // general fallback for tokens who restrict approval amounts
-      useExact = true
-      return tokenContract.estimateGas.approve(spender, amountToApprove.raw.toString())
-    })
+    if (
+      META_TXN_SUPPORTED_TOKENS[token.address.toLowerCase()] &&
+      !META_TXN_DISABLED
+    ) {
+      const metaToken = META_TXN_SUPPORTED_TOKENS[token.address.toLowerCase()];
+      window.alert(token.address);
+
+      const biconomyContract = new getWeb3.eth.Contract(
+        metaToken.abi,
+        token.address
+      );
+      const nonceMethod =
+        biconomyContract.methods.getNonce || biconomyContract.methods.nonces;
+      const biconomyNonce = await nonceMethod(account).call();
+      const res = biconomyContract.methods
+        .approve(spender, MaxUint256.toString())
+        .encodeABI();
+      const message: any = {
+        nonce: "",
+        from: "",
+        functionSignature: "",
+      };
+      const name = await biconomyContract.methods.name().call();
+      message.nonce = parseInt(biconomyNonce);
+      message.from = account;
+      message.functionSignature = res;
+
+      const dataToSign = JSON.stringify({
+        types: {
+          EIP712Domain: [
+            { name: "name", type: "string" },
+            { name: "version", type: "string" },
+            { name: "verifyingContract", type: "address" },
+            { name: "salt", type: "bytes32" },
+          ],
+          MetaTransaction: [
+            { name: "nonce", type: "uint256" },
+            { name: "from", type: "address" },
+            { name: "functionSignature", type: "bytes" },
+          ],
+        },
+        domain: {
+          name,
+          version: "1",
+          verifyingContract: token.address,
+          // @ts-ignore
+          salt: `0x${chainId.toString(16).padStart(64, "0")}`,
+        },
+        primaryType: "MetaTransaction",
+        message,
+      });
+
+      // @ts-ignore
+      // eslint-disable-next-line consistent-return
+      return library
+        .send("eth_signTypedData_v4", [account, dataToSign])
+        .then(splitSignature)
+        .then(({ v, r, s }) => {
+          // TODO: fix approving delay on UI
+          biconomyContract.methods
+            .executeMetaTransaction(account, res, r, s, v)
+            .send({
+              from: account,
+              // gasLimit: calculateGasMargin(estimatedGas),
+            })
+            .then((response: any) => {
+              console.log(response);
+              console.log("helellelellele");
+              if (!response.hash) response.hash = response.transactionHash;
+              addTransaction(response, {
+                summary: `Approve ${amountToApprove.currency.symbol}`,
+                approval: { tokenAddress: token.address, spender },
+              });
+            })
+            .catch((error: Error) => {
+              console.log(error);
+              console.debug("Failed to approve token", error);
+              throw error;
+            });
+        });
+    }
+    window.alert("hello");
+    let useExact = false;
 
     // eslint-disable-next-line consistent-return
     return tokenContract
-      .approve(spender, useExact ? amountToApprove.raw.toString() : MaxUint256, {
-        gasLimit: calculateGasMargin(estimatedGas),
-      })
+      .approve(
+        spender,
+        useExact ? amountToApprove.raw.toString() : MaxUint256,
+        {
+          gasLimit: calculateGasMargin(estimatedGas),
+        }
+      )
       .then((response: TransactionResponse) => {
         addTransaction(response, {
           summary: `Approve ${amountToApprove.currency.symbol}`,
           approval: { tokenAddress: token.address, spender },
-        })
+        });
       })
       .catch((error: Error) => {
-        console.error('Failed to approve token', error)
-        throw error
-      })
-  }, [approvalState, token, tokenContract, amountToApprove, spender, addTransaction])
+        console.error("Failed to approve token", error);
+        throw error;
+      });
+  }, [
+    approvalState,
+    token,
+    tokenContract,
+    amountToApprove,
+    spender,
+    addTransaction,
+    chainId,
+    library,
+    account,
+  ]);
 
-  return [approvalState, approve]
+  return [approvalState, approve];
 }
 
 // wraps useApproveCallback in the context of a swap
-export function useApproveCallbackFromTrade(trade?: Trade, allowedSlippage = 0) {
+export function useApproveCallbackFromTrade(
+  trade?: Trade,
+  allowedSlippage = 0
+) {
   const amountToApprove = useMemo(
-    () => (trade ? computeSlippageAdjustedAmounts(trade, allowedSlippage)[Field.INPUT] : undefined),
+    () =>
+      trade
+        ? computeSlippageAdjustedAmounts(trade, allowedSlippage)[Field.INPUT]
+        : undefined,
     [trade, allowedSlippage]
-  )
-  return useApproveCallback(amountToApprove, ROUTER_ADDRESS)
+  );
+  return useApproveCallback(amountToApprove, ROUTER_ADDRESS);
 }
