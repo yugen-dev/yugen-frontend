9f45a095857dafbe2cf3c96d171c8ae15f25a1e2
Karan <karankanthawar@gmail.com>
2021-07-01 14:53:57 +0530

    fix: Add white color to text when button is a danger variant

--- src/views/Swap/index.tsx
+++ src/views/Swap/index.tsx
@@ -560,7 +560,7 @@ const Swap = () => {
                     });
                   }
                 }}
-                style={{ width: "48%" }}
+                style={{ width: "48%", color: "white" }}
                 id="swap-button"
                 disabled={
                   !isValid ||
@@ -602,7 +602,7 @@ const Swap = () => {
                   ? "danger"
                   : "primary"
               }
-              style={{ maxWidth: "400px", width: "100%" }}
+              style={{ maxWidth: "400px", width: "100%", color: "white" }}
             >
               {swapInputError ||
                 (priceImpactSeverity > 3 && !isExpertMode
