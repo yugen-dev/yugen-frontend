91b5ebbaf0c9c203f0649f9bfccf2f0ed12bc945
Akshit Choudhary <akshitchoudhary007@gmail.com>
2021-08-03 13:45:16 +0530

    Fixed formula difference
    * Resolved formula difference  resulting  in different value between Home & staker page

--- src/views/CNTStaker/index.tsx
+++ src/views/CNTStaker/index.tsx
@@ -260,7 +260,7 @@ const CNTStaker = () => {
     valueOfCNTinUSD
   ) {
     cntStakingRatio =
-      (((parseFloat(dayDatas.data.dayDatas[1].volumeUSD) * 0.0005) /
+      (((parseFloat(dayDatas.data.dayDatas[1].volumeUSD) * 0.0005 * 0.35) /
         parseFloat(getCNTStakerInfo.data.cntstaker.totalSupply)) *
         365) /
       (parseFloat(getCNTStakerInfo.data.cntstaker.ratio) *
