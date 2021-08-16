040b06f4905bb2410f28be08c3738048bea1d4e7
samarth30 <samarth.gugnani30@gmail.com>
2021-08-16 19:33:49 +0530

    added pools for multireward

--- src/views/IFO/index.tsx
+++ src/views/IFO/index.tsx
@@ -51,7 +51,7 @@ const IFO = () => {
           <PoolsContainer>
             {openPools.map(
               (pool) =>
-                pool.sousId === 0 && (
+                pool.sousId === 3 && (
                   <PoolCard
                     key={pool.sousId}
                     pool={pool}
