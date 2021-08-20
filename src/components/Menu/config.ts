54d7ad75f3b302f4724f35dd1fdbc61ee0c07883
Prasenjit Kaninde <31060173+prasenjit-26@users.noreply.github.com>
2021-08-20 18:50:48 +0530

    anayltics sidebar changes

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -93,10 +93,6 @@ const config: MenuEntry[] = [
         href: "https://www.certik.org/projects/cryptionnetwork",
       },
       {
-        label: "Analytics",
-        href: "https://analytics.polydex.org/",
-      },
-      {
         label: "Github",
         href: "https://github.com/cryption-network",
       },
