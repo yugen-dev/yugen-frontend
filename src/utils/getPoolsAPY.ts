d29ae622d07e252c3228d7ee6b86afaa52905e8b
Karan-U-Kanthawar <karankanthawar@gmail.com>
2021-09-02 13:06:37 +0530

    fix: APR is calculated for all Pools including the loser tokens (LUSDT, LARTH)

--- src/utils/getPoolsAPY.ts
+++ src/utils/getPoolsAPY.ts
@@ -6,7 +6,7 @@ import { getBalanceNumber } from "utils/formatBalance";
 const calculatePoolsFunc = async (pool, prices, cntPrice) => {
   if (prices) {
     let rewardTokenCoinGeckoPrice = new BigNumber(1);
-    const pricefunc = async () => {
+    const getPriceFromCoinGecko = async () => {
       if (pool.rewardTokenCoinGeckoid.length > 0) {
         if (pool.rewardTokenCoinGeckoid === "CNT")
           rewardTokenCoinGeckoPrice = cntPrice;
@@ -16,38 +16,42 @@ const calculatePoolsFunc = async (pool, prices, cntPrice) => {
           );
       }
     };
-    await pricefunc();
+    await getPriceFromCoinGecko();
+
+    let tempStakingTokenPrice;
+    if (pool.tokenName === "LUSDT") tempStakingTokenPrice = 0.08;
+    if (pool.tokenName === "LARTH") tempStakingTokenPrice = 0.25;
+
     if (prices[`${pool.stakingTokenAddress}`.toLowerCase()]) {
-      let StakingTokenPrice =
+      tempStakingTokenPrice =
         prices[`${pool.stakingTokenAddress}`.toLowerCase()];
-      let apy = 0;
-
-      StakingTokenPrice = pool.tokenName === "LUSDT" ? 0.08 : StakingTokenPrice;
-      StakingTokenPrice = pool.tokenName === "LARTH" ? 0.25 : StakingTokenPrice;
-      pool.multiRewardTokenPerBlock.forEach(async (element, i) => {
-        const stakingTokenPrice = new BigNumber(StakingTokenPrice);
-        let tokenPrice;
-
-        if (pool.rewardTokenCoinGeckoid === "pear") {
-          tokenPrice = rewardTokenCoinGeckoPrice;
-        } else {
-          tokenPrice = prices[`${pool.coinGeckoIds[i]}`.toLowerCase()];
-        }
-
-        const rewardTokenPrice = tokenPrice
-          ? new BigNumber(tokenPrice)
-          : new BigNumber(1);
-
-        const currentTokenApy = getPoolApy(
-          stakingTokenPrice.toNumber(),
-          rewardTokenPrice.toNumber(),
-          getBalanceNumber(pool.totalStaked, pool.tokenDecimals),
-          parseFloat(element)
-        );
-        apy += currentTokenApy;
-      });
-      return apy;
     }
+
+    let apy = 0;
+
+    pool.multiRewardTokenPerBlock.forEach(async (element, i) => {
+      let tokenPrice;
+      const stakingTokenPrice = new BigNumber(tempStakingTokenPrice);
+
+      if (pool.rewardTokenCoinGeckoid === "pear") {
+        tokenPrice = rewardTokenCoinGeckoPrice;
+      } else {
+        tokenPrice = prices[`${pool.coinGeckoIds[i]}`.toLowerCase()];
+      }
+
+      const rewardTokenPrice = tokenPrice
+        ? new BigNumber(tokenPrice)
+        : new BigNumber(1);
+
+      const currentTokenApy = getPoolApy(
+        stakingTokenPrice.toNumber(),
+        rewardTokenPrice.toNumber(),
+        getBalanceNumber(pool.totalStaked, pool.tokenDecimals),
+        parseFloat(element)
+      );
+      apy += currentTokenApy;
+    });
+    return apy;
   }
   return 0;
 };
