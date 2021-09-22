e3a2c947060926e8c026f3bc7f23dacc6db58874
Karan-U-Kanthawar <karankanthawar@gmail.com>
2021-09-22 12:50:12 +0530

    feat: Remove unused pools & multirewards calls in multicall

--- src/hooks/useAllEarnings.ts
+++ src/hooks/useAllEarnings.ts
@@ -1,11 +1,9 @@
 import { useEffect, useState } from "react";
 import { useWeb3React } from "@web3-react/core";
 import multicall from "utils/multicall";
-import { getHybridStakingContract } from "utils/contractHelpers";
-import { getFarmAddress, getAddress } from "utils/addressHelpers";
+import { getFarmAddress } from "utils/addressHelpers";
 import farmABI from "config/abi/farm.json";
-import sousChefABI from "config/abi/sousChef.json";
-import { farmsConfig, poolsConfig } from "config/constants";
+import { farmsConfig } from "config/constants";
 import useRefresh from "./useRefresh";
 
 const useAllEarnings = () => {
@@ -20,27 +18,28 @@ const useAllEarnings = () => {
         name: "pendingYGN",
         params: [farm.pid, account],
       }));
-      const poolCalls = [];
-      const poolsWithEnd = poolsConfig.filter((p) => p.sousId !== 0);
-      // eslint-disable-next-line array-callback-return
-      poolsWithEnd.map((pool) => {
-        if (pool.multiReward.indexOf("YGN") > -1) {
-          poolCalls.push({
-            address: getAddress(pool.contractAddress),
-            name: "pendingReward",
-            params: [account, pool.multiReward.indexOf("YGN")],
-          });
-        }
-      });
+      // const poolCalls = [];
+      // const poolsWithEnd = poolsConfig.filter((p) => p.sousId !== 0);
+      // // eslint-disable-next-line array-callback-return
+      // poolsWithEnd.map((pool) => {
+      //   if (pool.multiReward.indexOf("YGN") > -1) {
+      //     poolCalls.push({
+      //       address: getAddress(pool.contractAddress),
+      //       name: "pendingReward",
+      //       params: [account, pool.multiReward.indexOf("YGN")],
+      //     });
+      //   }
+      // });
 
       const res = await multicall(farmABI, calls);
-      const resPools = await multicall(sousChefABI, poolCalls);
-      const contract = getHybridStakingContract();
-      const pendingRewardHybridStaking = await contract.methods
-        .pendingYGN("0", account)
-        .call();
-      const response = res.concat(resPools).concat(pendingRewardHybridStaking);
-      setBalance(response);
+      // const resPools = await multicall(sousChefABI, poolCalls);
+      // const contract = getHybridStakingContract();
+      // const pendingRewardHybridStaking = await contract.methods
+      //   .pendingYGN("0", account)
+      //   .call();
+      // const response = res.concat(resPools).concat(pendingRewardHybridStaking);
+      // setBalance(response);
+      setBalance(res);
     };
 
     if (account) {
