e3ac929a7642eef52d71632ef12125e7617efe98
Karan-U-Kanthawar <karankanthawar@gmail.com>
2021-10-06 08:07:54 +0530

    feat: Finish UI connections

--- src/views/Vaults/components/FarmTable/Actions/ActionPanel.tsx
+++ src/views/Vaults/components/FarmTable/Actions/ActionPanel.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import styled from "styled-components";
 import useI18n from "hooks/useI18n";
 import { LinkExternal, Text } from "cryption-uikit";
 import { VaultWithStakedValue } from "views/Farms/components/FarmCard/FarmCard";
 import { BigNumber } from "bignumber.js";
+import { fetchPrice, usePriceCakeBusd } from "state/hooks";
+import { getBalanceNumber } from "utils/formatBalance";
+import { getAddress } from "utils/addressHelpers";
 import StakedAction from "./StakedAction";
 import Apr, { AprProps } from "../Apr";
 import Liquidity, { LiquidityProps } from "../Liquidity";
@@ -90,6 +93,8 @@ const ActionPanel: React.FunctionComponent<ActionPanelProps> = ({
   liquidity,
   wallet,
 }) => {
+  const [arrayOfTokenBalances, setArrayOfTokenBalances] = useState(["0", "0"]);
+  const ygnPrice = usePriceCakeBusd();
   const vault = details;
   const priceOf1LP = new BigNumber(liquidity.liquidity)
     .dividedBy(vault.totalLPTokensStakedInFarms)
@@ -103,16 +108,50 @@ const ActionPanel: React.FunctionComponent<ActionPanelProps> = ({
   const TranslateString = useI18n();
   const lpLabel =
     vault.lpTokenName && vault.lpTokenName.toUpperCase().replace("PANCAKE", "");
-  const liquidityUrlPathParts = "";
-  const bsc = "";
+  const liquidityUrlPathParts = `${getAddress(
+    vault.lpTokenPart1Address
+  )}/${getAddress(vault.lpTokenPart1Address)}`;
+  const bsc = `https://polygonscan.com/address/${getAddress(
+    vault.lpTokenAddress
+  )}`;
   const splitLP = vault.lpTokenName.split("-");
+
+  const getLpPrices = useCallback(
+    async (arrayOfLP) => {
+      const arrayOfLpPrices = new Array(2);
+      arrayOfLpPrices[0] = await fetchPrice(arrayOfLP[0]);
+      if (arrayOfLP[1] === "YGN") arrayOfLpPrices[1] = ygnPrice;
+      else arrayOfLpPrices[1] = await fetchPrice(arrayOfLP[1]);
+
+      if (vault?.userData) {
+        const lpTokenBalancesInUSD = new Array(2);
+        lpTokenBalancesInUSD[0] = new BigNumber(arrayOfLpPrices[0])
+          .multipliedBy(getBalanceNumber(vault.userData.firstLpTokenBalance))
+          .toFixed(2)
+          .toString();
+        lpTokenBalancesInUSD[1] = new BigNumber(arrayOfLpPrices[1])
+          .multipliedBy(getBalanceNumber(vault.userData.secondLpTokenBalance))
+          .toFixed(2)
+          .toString();
+
+        setArrayOfTokenBalances(lpTokenBalancesInUSD);
+      }
+    },
+    [vault.userData, ygnPrice]
+  );
+
+  useEffect(() => {
+    if (splitLP.length === 2) getLpPrices(splitLP);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <Container>
       <InfoContainer>
         <StakeContainer>
           <StyledLinkExternal
-            href={`https://app.polydex.org/add/ETH/${liquidityUrlPathParts}`}
+            href={`https://app.polydex.org/add/${liquidityUrlPathParts}`}
           >
             {TranslateString(999, `Get  ${lpLabel}`, { name: lpLabel })}
           </StyledLinkExternal>
@@ -120,18 +159,33 @@ const ActionPanel: React.FunctionComponent<ActionPanelProps> = ({
         <StyledLinkExternal href={bsc}>
           {TranslateString(999, "PolygonScan")}
         </StyledLinkExternal>
+
         <StyledText>
-          Deposited: {vault?.userData?.stakedBalance} {vault?.lpTokenName} LP
+          Deposited:{" "}
+          {getBalanceNumber(
+            new BigNumber(vault?.userData?.stakedBalance)
+          ).toFixed(2)}{" "}
+          {vault?.lpTokenName} LP
         </StyledText>
+
         <StyledText>
-          {splitLP[0]} balance: {vault?.userData?.firstLpTokenBalance}{" "}
-          {splitLP[0]} ($10000)
+          {splitLP[0]} Balance:{" "}
+          {getBalanceNumber(
+            new BigNumber(vault?.userData?.firstLpTokenBalance)
+          ).toFixed(2)}{" "}
+          {splitLP[0]} ($
+          {arrayOfTokenBalances[0]})
         </StyledText>
+
         <StyledText>
-          {splitLP[1]} balance: {vault?.userData?.secondLpTokenBalance}{" "}
-          {splitLP[1]} ($5000)
+          {splitLP[1]} Balance:{" "}
+          {getBalanceNumber(
+            new BigNumber(vault?.userData?.secondLpTokenBalance)
+          ).toFixed(2)}
+          {splitLP[1]} (${arrayOfTokenBalances[1]})
         </StyledText>
-        <StyledText>Total balance: ${totalBalanceValue.toString()}</StyledText>
+
+        <StyledText>Total Balance: ${totalBalanceValue.toString()}</StyledText>
         <StyledText>
           In Wallet: {wallet?.value} {vault?.lpTokenName} LP
         </StyledText>
