3534973986ffb63e4ec8a22bfaca0da5aa1f6b02
Karan <karankanthawar@gmail.com>
2022-01-07 18:51:45 +0530

    feat: Update contract details

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -3,7 +3,59 @@ import { FarmConfig, QuoteToken } from "./types";
 
 const farmsEthereumMainnet: FarmConfig[] = [];
 const farmsEthereumTestnet: FarmConfig[] = [];
-const farmsMaticMainnet: FarmConfig[] = [];
+const farmsMaticMainnet: FarmConfig[] = [
+  {
+    pid: 0,
+    tag: "Powered by QuickSwap",
+    lpSymbol: "QUICK-wMATIC",
+    lpAddresses: {
+      97: "0x86652c1301843B4E06fBfbBDaA6849266fb2b5e7",
+      56: "0x86652c1301843B4E06fBfbBDaA6849266fb2b5e7",
+      80001: "0x978d374800eb7861283ffa0326bb4c853045b919", // QUICK-wMATIC
+      5: "0x86652c1301843B4E06fBfbBDaA6849266fb2b5e7",
+      137: "0x019ba0325f1988213D448b3472fA1cf8D07618d7",
+      1: "0x86652c1301843B4E06fBfbBDaA6849266fb2b5e7",
+      250: "0x401e9E359d6De9B313c85Cde095D61b42B96EBEd",
+      4002: "0x401e9E359d6De9B313c85Cde095D61b42B96EBEd",
+    },
+    tokenSymbol: "SUPER",
+    volatility: "failure",
+    tokenAddresses: {
+      97: "0x86652c1301843B4E06fBfbBDaA6849266fb2b5e7",
+      56: "0x86652c1301843B4E06fBfbBDaA6849266fb2b5e7",
+      80001: "0x9293C7381b9cEA6B27Ce9069f26746e7D43bC29d", // QUICK
+      5: "0x86652c1301843B4E06fBfbBDaA6849266fb2b5e7",
+      137: "0xf28164a485b0b2c90639e47b0f377b4a438a16b1",
+      1: "0x86652c1301843B4E06fBfbBDaA6849266fb2b5e7",
+      250: "0x401e9E359d6De9B313c85Cde095D61b42B96EBEd",
+      4002: "0x401e9E359d6De9B313c85Cde095D61b42B96EBEd",
+    },
+    quoteTokenSymbol: QuoteToken.WMATIC,
+    quoteTokenAdresses: contracts.wbnb,
+    singleSidedToken: {
+      97: "0x86652c1301843B4E06fBfbBDaA6849266fb2b5e7",
+      56: "0x86652c1301843B4E06fBfbBDaA6849266fb2b5e7",
+      80001: "0x9c3c9283d3e44854697cd22d3faa240cfb032889", // wMATIC
+      5: "0x86652c1301843B4E06fBfbBDaA6849266fb2b5e7",
+      137: "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270",
+      1: "0x86652c1301843B4E06fBfbBDaA6849266fb2b5e7",
+      250: "0x401e9E359d6De9B313c85Cde095D61b42B96EBEd",
+      4002: "0x401e9E359d6De9B313c85Cde095D61b42B96EBEd",
+    },
+    singleSidedTokenName: "WMATIC",
+    singleSidedToToken: {
+      97: "0x86652c1301843B4E06fBfbBDaA6849266fb2b5e7",
+      56: "0x86652c1301843B4E06fBfbBDaA6849266fb2b5e7",
+      80001: "0x9293C7381b9cEA6B27Ce9069f26746e7D43bC29d", // QUICK
+      5: "0x86652c1301843B4E06fBfbBDaA6849266fb2b5e7",
+      137: "0xf28164a485b0b2c90639e47b0f377b4a438a16b1",
+      1: "0x86652c1301843B4E06fBfbBDaA6849266fb2b5e7",
+      250: "0x401e9E359d6De9B313c85Cde095D61b42B96EBEd",
+      4002: "0x401e9E359d6De9B313c85Cde095D61b42B96EBEd",
+    },
+    singleSidedToTokenName: "QUICK",
+  },
+];
 const farmsMaticTestnet: FarmConfig[] = [
   {
     pid: 0,
